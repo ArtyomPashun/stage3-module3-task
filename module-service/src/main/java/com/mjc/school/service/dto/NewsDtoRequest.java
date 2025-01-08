6d40b1de82a56210a669f013b15c588ceb608305
Artsiom Pashun <artsiom.pashun@leverx.com>
2025-01-08 17:25:48 +0100

    Add DTOs for TagRequest and TagResponse

--- module-service/src/main/java/com/mjc/school/service/dto/NewsDtoRequest.java
+++ module-service/src/main/java/com/mjc/school/service/dto/NewsDtoRequest.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +14,5 @@ public class NewsDtoRequest {
     private String title;
     private String content;
     private long authorId;
+    private List<Long> tags;
 }
