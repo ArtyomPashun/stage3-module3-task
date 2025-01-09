921247a043fc993b3d1d13832def9e266a5f77de
Artsiom Pashun <artsiom.pashun@leverx.com>
2025-01-09 16:44:14 +0100

    Update configs

--- module-main/src/main/java/com/mjc/school/main/Main.java
+++ module-main/src/main/java/com/mjc/school/main/Main.java
@@ -1,8 +1,8 @@
 package com.mjc.school.main;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @PropertySource("classpath:application.properties")
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        ApplicationContext context = SpringApplication.run(Main.class, args);
         Menu menu = context.getBean("menu", Menu.class);
         menu.printMenu();
     }
