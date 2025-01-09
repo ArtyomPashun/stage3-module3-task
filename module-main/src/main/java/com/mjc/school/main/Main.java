cf6d0914b96a8954dab2043e23161b66872ac86f
Artsiom Pashun <artsiom.pashun@leverx.com>
2025-01-09 16:26:59 +0100

    Update configs

--- module-main/src/main/java/com/mjc/school/main/Main.java
+++ module-main/src/main/java/com/mjc/school/main/Main.java
@@ -1,8 +1,16 @@
 package com.mjc.school.main;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableJpaAuditing
+@PropertySource("classpath:application.properties")
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
